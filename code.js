fb9c6e0a4220961a7538c61b9267c1bc36c7da7a
Michel <michl521521@gmail.com>
2022-07-15 17:04:49 -0500

    actualizando diseño

--- code.js
+++ code.js
@@ -191,7 +191,7 @@ document.getElementById("recoRestaurante").addEventListener("click",()=>{
    document.getElementById("tiempoPedidoInp").focus();
 })
 document.getElementById("tiempoPedidoInp").addEventListener("change", e =>{
-   console.log(e.target.value);
+   // console.log(e.target.value);
    llenarLink(msjComplete.concat(tmpmsj + "*Recojo en el Restaurante*").concat(".  Llego aproximadamente a las: *").concat(e.target.value).concat("*") ,2)
 })
 document.querySelectorAll(".dialog").forEach(di =>{
