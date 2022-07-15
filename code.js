9d5f6692d13f9378b8d6a3e7fcc4a77a3ff7573a
Michel <michl521521@gmail.com>
2022-07-14 19:26:57 -0500

    Primera version definitiva, en busca de posibles errores

--- code.js
+++ code.js
@@ -1,341 +1,3 @@
-let obj = [
-   {
-      name: "sushi frio",
-      imag: "media/sushi.png",
-      data: [
-         {
-            plat: "salmontino",
-            ingr: "Pasta tampico, philadelphia, pepino, zanahoria, por fuera salmón ahumado y aguacate.",
-            cost: 85
-         }, {
-            plat: "kioto roll",
-            ingr: "Salmon sellado, philadelphia, pasta tampico y aguacate.",
-            cost: 85
-         }, {
-            plat: "cangrejo roll",
-            ingr: "Surimi empanizado, pepino, aguacate, alga por fuera y champiñon.",
-            cost: 75
-         }, {
-            plat: "cheese roll",
-            ingr: "Forrado de queso manchego. aderezo de chipotle, camaron empanizado , philadelphia y aguacate",
-            cost: 75
-         }, {
-            plat: "dieta roll",
-            ingr: "Surimi, philadelphia, aguacate envuelto en hoja de pepino.",
-            cost: 75
-         }, {
-            plat: "loco roll",
-            ingr: "Camaron, philadelphia, aguacate envuelto en tocino con queso manchego gratinado",
-            cost: 75
-         }, {
-            plat: "macho roll",
-            ingr: "Camaron empanizado, aguacate, philadelphia, envuelto en plátano frito y salsa de anguila.",
-            cost: 70
-         }, {
-            plat: "green roll",
-            ingr: "Camaron, pepino, aguacate, envuelto en kiwi con salsa de anguila.",
-            cost: 70
-         }, {
-            plat: "amai maki",
-            ingr: "Surimi empanizado, aguacate, philadelphia, pepino por fuera con salsa dulce picante.",
-            cost: 70
-         }, {
-            plat: "kanikama maki",
-            ingr: "Surimi, pasta tampico por dentro y por fuera.",
-            cost: 70
-         }, {
-            plat: "tropical roll",
-            ingr: "Surimi, pepino, philadelphia, envuelto en mango con salsa de anguila.",
-            cost: 65
-         }, {
-            plat: "palto maki",
-            ingr: "Camarón, pepino, philadelphia, aguacate por fuera y ajonjolí.",
-            cost: 65
-         }, {
-            plat: "california maki",
-            ingr: "Camaron, pepino, aguacate y ajonjolí por fuera.",
-            cost: 55
-         }
-      ]
-   },
-   {
-      name: "sushi empanizado",
-      imag: "media/sushi3.png",
-      data: [
-         {
-            plat: "delux roll",
-            ingr: "Camaron empanizado, aguacate, philadelphia, empanizado por fuera gratinado con queso manchego al chipotle y tocino.",
-            cost: 85
-         },
-         {
-            plat: "mar y tierra",
-            ingr: "Camaron, res, pollo en teriyaki, manchego, empanizado por fuera.",
-            cost: 80
-         },
-         {
-            plat: "dragon roll",
-            ingr: "Res a la plancha, queso manchego, chiles toreados, empanizado por fuera.",
-            cost: 80
-         },
-         {
-            plat: "coco roll",
-            ingr: "Surimi empanizado, aguacate, philadelphia, empanizado por fuera y dulce de coco.",
-            cost: 75
-         },
-         {
-            plat: "sweet roll",
-            ingr: "Pollo en teriyaki, philadelphia, queso manchego, empanizado.",
-            cost: 75
-         },
-         {
-            plat: "hot roll",
-            ingr: "Camaron empanizado, aguacate, philadelphia, empanizado por fuera y chipotle.",
-            cost: 70
-         },
-         {
-            plat: "dishi maki",
-            ingr: "Surimi empanizado, aguacate, queso manchego con salsa dulce picante, empanizado por fuera.",
-            cost: 75
-         },
-         {
-            plat: "queso maki",
-            ingr: "Philadelphia, queso manchego, empanizado. por fuera",
-            cost: 60
-         }
-      ]
-   },
-   {
-      name: "yakimeshi",
-      imag: "media/sopa.png",
-      data: [
-         {
-            plat: "yakimeshi verdura",
-            ingr: "arroz frito, zanahoria, calabaza, huevo.",
-            cost: 45
-         },
-         {
-            plat: "yakimeshi hiyoko (pollo)",
-            ingr: "arroz frito, pollo, zanahoria, calabaza, huevo.",
-            cost: 50
-         },
-         {
-            plat: "yakimeshi gyuniku (res)",
-            ingr: "arroz frito, res, zanahoria, calabaza, huevo.",
-            cost: 55
-         },
-         {
-            plat: "yakimeshi surimi",
-            ingr: "arroz frito, surimi, zanahoria, calabaza, huevo.",
-            cost: 60
-         }
-      ]
-   },
-   {
-      name: "pastas",
-      imag: "media/ramen.png",
-      data: [
-         {
-            plat: "pasta schezuan hiyoko",
-            ingr: "pasta, pollo, zanahoria, calabaza, brocoli, champiñones, pimientos y cebollin.",
-            cost: 65
-         },
-         {
-            plat: "pasta schezuan gyuniku",
-            ingr: "pasta, res, zanahoria, calabaza, brocoli, champiñones, pimientos y cebollin.",
-            cost: 70
-         }
-      ]
-   },
-   {
-      name: "ramen",
-      imag: "media/ramen2.png",
-      data: [
-         {
-            plat: "ramen res",
-            ingr: "res, huevo, naruto, cebollin, zanahoria, calabaza, pasta ramen",
-            cost: 75
-         },
-         {
-            plat: "ramen surimi",
-            ingr: "surimi en tempura, huevo, naruto, cebollin, zanahoria, calabaza, pasta ramen.",
-            cost: 75
-         }
-      ]
-   },
-   {
-      name: "teppanyaki ",
-      imag: "media/teppanyaki.png",
-      data: [
-         {
-            plat: "teppanyaki verdura",
-            ingr: "verduras a la plancha, champiñones, calabaza, zanahoria, brocoli, pimiento, cebollin y arroz gohan.",
-            cost: 45
-         },
-         {
-            plat: "teppanyaki hiyoko (pollo)",
-            ingr: "verduras a la plancha, pollo, champiñones, calabaza, zanahoria, brocoli, pimiento, cebollin y arroz gohan.",
-            cost: 50
-         },
-         {
-            plat: "teppanyaki gyuniku",
-            ingr: "verduras a la plancha, res, champiñones, zanahoria, brocoli, pimiento y cebollin.",
-            cost: 55
-         },
-      ]
-   },
-   {
-      name: "temaki",
-      imag: "media/temaki.png",
-      data: [
-         {
-            plat: "temaki kappa-shako",
-            ingr: "camaron, philadephia, pepino, zanahoria, ajonjolí.",
-            cost: 55
-         },
-         {
-            plat: "temaki vegetariano",
-            ingr: "pepino, zanahoria, aguacate, philadelphia.",
-            cost: 50
-         },
-      ]
-   },
-   {
-      name: "kushiague",
-      imag: "media/poke.png",
-      data: [
-         {
-            plat: "kushi queso",
-            ingr: "brocheta de queso manchego, philadelphia, empanizado.",
-            cost: 35
-         },
-         {
-            plat: "kushi ebi",
-            ingr: "brocheta de camaron, philadelphia, empanizado.",
-            cost: 35
-         },
-         {
-            plat: "kushi surim ",
-            ingr: "brocheta de surimi, philadelphia, empanizado.",
-            cost: 30
-         },
-         {
-            plat: "kushi banan ",
-            ingr: "brocheta de plátano frito, philadelphia, empanizado.",
-            cost: 25
-         }
-      ]
-   },
-   {
-      name: "gyozas",
-      imag: "media/gyoza.png",
-      data: [
-         {
-            plat: "gyozas",
-            ingr: "Rellenas de pollo sabor schezuan acompañado con verduras.",
-            cost: 55
-         }
-      ]
-   },
-   {
-      name: "tempura",
-      imag: "media/poke.png",
-      data: [
-         {
-            plat: "roshy roll",
-            ingr: "surimi, aguacate por fuera en tempura con pasta tampico.",
-            cost: 80
-         },
-      ]
-   },
-   {
-      name: "sushi charola",
-      imag: "media/bento.png",
-      data: [
-         {
-            plat: "sushi empanizados",
-            ingr: "charola sushi empanizado 30 piezas",
-            cost: 200
-         },
-         {
-            plat: "sushi surtidos",
-            ingr: "charola sushi empanizado y frio 40 piezas",
-            cost: 350
-         },
-         {
-            plat: "sushi surtidos",
-            ingr: "charola sushi empanizado y frio 30 piezas",
-            cost: 270
-         }
-      ]
-   },
-   {
-      name: "poke",
-      imag: "media/poke.png",
-      data: [
-         {
-            plat: "poke amai toriniku",
-            ingr: "pollo tempura en teriyaki, mango, pepino, ajonjolin, arroz gohan y alga nori.",
-            cost: 65
-         },
-         {
-            plat: "poke surimi terivyaki",
-            ingr: "surimi tempura en teriyaki, mango, pepino, ajonjolin, arroz gohan y alga nori.",
-            cost: 65
-         }
-      ]
-   },
-   {
-      name: "bebidas",
-      imag: "media/beer.png",
-      data: [
-         { plat: "nieve flotante limon", cost: 38 },
-         { plat: "jarra de te helado", ingr:"Negro", cost: 40 },
-         { plat: "vaso de te helado", ingr:"Negro", cost: 15 },
-         { plat: "jarra de calpiña", cost: 50 },
-         { plat: "vaso de calpiña", cost: 25 },
-         { plat: "refresco lata", cost: 20 },
-         { plat: "sake", cost: 15 },
-      ]
-   },
-   {
-      name: "postres",
-      imag: "media/narutomaki.png",
-      data: [
-         {
-            plat: "tempura helado classic",
-            ingr: "cubierto con chocolate líquido.",
-            cost: 40
-         },
-         {
-            plat: "tempura helado crunchy",
-            ingr: "cubierto con cajeta y nuez.",
-            cost: 45
-         },
-         {
-            plat: "tempura helado magic",
-            ingr: "cubierto con chocolate líquido, crema batida, cereza y con bola de nieve sabor chocolate.",
-            cost: 50
-         },
-         {
-            plat: "nieve de limon",
-            cost: 20
-         }
-      ]
-   },
-   {
-      name: "extras",
-      imag: "media/takoyaki.png",
-      data: [
-         { plat: "extra salsa de anguila", cost: 10 },
-         { plat: "extra aderezo de chipotle", cost: 10 },
-         { plat: "extra pasta tampco", cost: 15 },
-         { plat: "extra wasabi", cost: 10 },
-         { plat: "chiles toreados", cost: 10 },
-         { plat: "extra wasabi", cost: 10 },
-         { plat: "ingrediente extra", cost: 10 }
-      ]
-   },
-]
 const ordenesComponent = ({plat,pre,pz,tot}) =>{
    return `
       <div class="ordenMainCon">
@@ -395,6 +57,9 @@ const componente = (obj, long) => {
 const scale = (el,bol) =>{
    el.style.transform= `scale(${bol ? '1' : '0'})`
 }
+let msjComplete='';
+let tmpmsj = "";
+
 const navegacion = document.getElementById("navegacion");
 const platillos = document.getElementById("platillos");
 const deposito = document.getElementById("deposito");
@@ -404,6 +69,26 @@ const inputPlatillos = document.getElementById("inputPlatillos");
 const formPlatillos = document.getElementById("formPlatillos");
 const cantEfectivo = document.getElementById("cantEfectivo");
 let elementActual;
+
+const llenarLink = (val,num,bol) => {
+   const apiWhatsapp = 'https://api.whatsapp.com/send?phone=527491060297&text='
+   let el = document.querySelectorAll(".linkWhats")
+   // el.forEach(e => console.log(e.getAttribute("data-type")) )
+   let ar = [el[0],el[1],el[2]]
+
+   
+   if(!bol){
+      val = "Hola me gustaria ordenar:+".concat(val)
+   }
+   val = val.replaceAll(' ','+')
+   
+   // console.log(val);
+   // console.log(ar)
+   ar[num].setAttribute("href",apiWhatsapp.concat(val))
+   // console.log(ar[0]);
+   // console.log(ar[1]);
+   // console.log(ar[2]);
+}
 
 window.addEventListener("submit", e =>{
    e.preventDefault()
@@ -439,18 +124,6 @@ obj.map(o => {
       navegacion.lastElementChild.lastElementChild.innerHTML += subComponent(d)
    })
 })
-// document.querySelectorAll(".downCotent").forEach(dc => {
-//    dc.addEventListener("click", () => {
-//       let padre = dc.parentElement.parentElement;
-//       padre.classList.contains("desplegado") ? [
-//          padre.style.height = "58px",
-//          padre.classList.remove("desplegado")
-//       ] : [
-//          padre.style.height = `${padre.scrollHeight + 10}px`,
-//          padre.classList.add("desplegado")
-//       ]
-//    })
-// })
 document.querySelectorAll(".navegacion section > div").forEach(dl => {
    dl.addEventListener("click", () => {
       let padre = dl.parentElement;
@@ -463,7 +136,6 @@ document.querySelectorAll(".navegacion section > div").forEach(dl => {
       ]
    })
 })
-
 document.querySelectorAll("#navegacion > section nav div").forEach(pl => {
    pl.addEventListener("click", e => {
       // console.clear()
@@ -495,11 +167,13 @@ document.querySelectorAll("#navegacion > section nav div").forEach(pl => {
    })
 })
 document.getElementById("abrirOrden").addEventListener("click", ()=>{
+   msjComplete='';
    let con = document.getElementById("ordenMain")
    con.innerHTML="";
    let tot=0;
    let totfin=0;
    document.querySelectorAll(".ordenado").forEach(or => {
+      msjComplete+= or.getAttribute("data-plat") + "-" + or.getAttribute("data-name")+", ";
       tot= parseInt(or.getAttribute("data-cost")) * parseInt(or.getAttribute("data-plat"))
       totfin+=tot
       con.innerHTML+=ordenesComponent({
@@ -515,6 +189,8 @@ document.getElementById("abrirOrden").addEventListener("click", ()=>{
    } else{
       alert("Por favor ordene algo")
    }
+   llenarLink(msjComplete.concat("Recojo en el restaurante"),2)
+   // console.log(msjComplete);
 })
 document.getElementById("opcPago").addEventListener("click", e =>{
    let tar = e.target
@@ -525,15 +201,31 @@ document.getElementById("selecPagoSpan").addEventListener("click", e =>{
    if(tar.tagName=="BUTTON"){
       ((tar.getAttribute("id")=="pagoTransferencia") 
       ? deposito : cantEfectivo).style.transform= `scale(1)`
+
+      // console.log(document.getElementById("datosDomicilio"));
+      // console.log(document.getElementById("datosDomicilio")[0]);
+      // console.log(document.getElementById("datosDomicilio")[0].value);
+
+      tmpmsj = `vivo por: ${document.getElementById("datosDomicilio")[0].value} a nombre de: ${document.getElementById("datosDomicilio")[1].value} pago mediante: `
+
+      llenarLink(msjComplete.concat(tmpmsj.concat((tar.getAttribute("id")=="pagoTransferencia") ? "transferencia " : "efectivo ")),
+      ((tar.getAttribute("id")=="pagoTransferencia") ? 0 : 1)
+      )
    }
 })
-domicilio.addEventListener("click", e =>{
-   if(e.target.getAttribute("id")=="domicilio"){
-      domicilio.style.transform=`scale(0)`
-   }
+// document.querySelectorAll(".linkWhats")[1].setAttribute("href","###")
+// console.log(document.querySelectorAll(".linkWhats")[1].getAttribute("href"));
+document.getElementById("cantEfectivoVal").addEventListener("keyup", e =>{
+   llenarLink(msjComplete.concat(tmpmsj).concat(e.target.value),1,true)
+   // console.log();
+   // console.log(e.target);
+   // let el = document.querySelectorAll(".linkWhats")[1]
+   // let at = el.getAttribute("href");
+   // el.setAttribute("href",.concat(e.target.value))
+   // console.log(el.getAttribute("href"));
 })
+domicilio.addEventListener("click", e =>{if(e.target.getAttribute("id")=="domicilio"){domicilio.style.transform=`scale(0)`}})
 deposito.addEventListener("click", e =>{if(e.target.getAttribute("id")=="deposito"){deposito.style.transform=`scale(0)`}})
 orden.addEventListener("click", e =>{if(e.target.getAttribute("id")=="orden"){orden.style.transform=`scale(0)`}})
 platillos.addEventListener("click", e =>{if(e.target.getAttribute("id")=="platillos"){platillos.style.transform=`scale(0)`}})
-
 cantEfectivo.addEventListener("click", e =>{if(e.target.getAttribute("id")=="cantEfectivo"){cantEfectivo.style.transform=`scale(0)`}})
---
+++ data.js
@@ -0,0 +1,338 @@
+let obj = [
+   {
+      name: "sushi frio",
+      imag: "media/sushi.png",
+      data: [
+         {
+            plat: "salmontino",
+            ingr: "Pasta tampico, philadelphia, pepino, zanahoria, por fuera salmón ahumado y aguacate.",
+            cost: 85
+         }, {
+            plat: "kioto roll",
+            ingr: "Salmon sellado, philadelphia, pasta tampico y aguacate.",
+            cost: 85
+         }, {
+            plat: "cangrejo roll",
+            ingr: "Surimi empanizado, pepino, aguacate, alga por fuera y champiñon.",
+            cost: 75
+         }, {
+            plat: "cheese roll",
+            ingr: "Forrado de queso manchego. aderezo de chipotle, camaron empanizado , philadelphia y aguacate",
+            cost: 75
+         }, {
+            plat: "dieta roll",
+            ingr: "Surimi, philadelphia, aguacate envuelto en hoja de pepino.",
+            cost: 75
+         }, {
+            plat: "loco roll",
+            ingr: "Camaron, philadelphia, aguacate envuelto en tocino con queso manchego gratinado",
+            cost: 75
+         }, {
+            plat: "macho roll",
+            ingr: "Camaron empanizado, aguacate, philadelphia, envuelto en plátano frito y salsa de anguila.",
+            cost: 70
+         }, {
+            plat: "green roll",
+            ingr: "Camaron, pepino, aguacate, envuelto en kiwi con salsa de anguila.",
+            cost: 70
+         }, {
+            plat: "amai maki",
+            ingr: "Surimi empanizado, aguacate, philadelphia, pepino por fuera con salsa dulce picante.",
+            cost: 70
+         }, {
+            plat: "kanikama maki",
+            ingr: "Surimi, pasta tampico por dentro y por fuera.",
+            cost: 70
+         }, {
+            plat: "tropical roll",
+            ingr: "Surimi, pepino, philadelphia, envuelto en mango con salsa de anguila.",
+            cost: 65
+         }, {
+            plat: "palto maki",
+            ingr: "Camarón, pepino, philadelphia, aguacate por fuera y ajonjolí.",
+            cost: 65
+         }, {
+            plat: "california maki",
+            ingr: "Camaron, pepino, aguacate y ajonjolí por fuera.",
+            cost: 55
+         }
+      ]
+   },
+   {
+      name: "sushi empanizado",
+      imag: "media/sushi3.png",
+      data: [
+         {
+            plat: "delux roll",
+            ingr: "Camaron empanizado, aguacate, philadelphia, empanizado por fuera gratinado con queso manchego al chipotle y tocino.",
+            cost: 85
+         },
+         {
+            plat: "mar y tierra",
+            ingr: "Camaron, res, pollo en teriyaki, manchego, empanizado por fuera.",
+            cost: 80
+         },
+         {
+            plat: "dragon roll",
+            ingr: "Res a la plancha, queso manchego, chiles toreados, empanizado por fuera.",
+            cost: 80
+         },
+         {
+            plat: "coco roll",
+            ingr: "Surimi empanizado, aguacate, philadelphia, empanizado por fuera y dulce de coco.",
+            cost: 75
+         },
+         {
+            plat: "sweet roll",
+            ingr: "Pollo en teriyaki, philadelphia, queso manchego, empanizado.",
+            cost: 75
+         },
+         {
+            plat: "hot roll",
+            ingr: "Camaron empanizado, aguacate, philadelphia, empanizado por fuera y chipotle.",
+            cost: 70
+         },
+         {
+            plat: "dishi maki",
+            ingr: "Surimi empanizado, aguacate, queso manchego con salsa dulce picante, empanizado por fuera.",
+            cost: 75
+         },
+         {
+            plat: "queso maki",
+            ingr: "Philadelphia, queso manchego, empanizado. por fuera",
+            cost: 60
+         }
+      ]
+   },
+   {
+      name: "yakimeshi",
+      imag: "media/sopa.png",
+      data: [
+         {
+            plat: "yakimeshi verdura",
+            ingr: "arroz frito, zanahoria, calabaza, huevo.",
+            cost: 45
+         },
+         {
+            plat: "yakimeshi hiyoko (pollo)",
+            ingr: "arroz frito, pollo, zanahoria, calabaza, huevo.",
+            cost: 50
+         },
+         {
+            plat: "yakimeshi gyuniku (res)",
+            ingr: "arroz frito, res, zanahoria, calabaza, huevo.",
+            cost: 55
+         },
+         {
+            plat: "yakimeshi surimi",
+            ingr: "arroz frito, surimi, zanahoria, calabaza, huevo.",
+            cost: 60
+         }
+      ]
+   },
+   {
+      name: "pastas",
+      imag: "media/ramen.png",
+      data: [
+         {
+            plat: "pasta schezuan hiyoko",
+            ingr: "pasta, pollo, zanahoria, calabaza, brocoli, champiñones, pimientos y cebollin.",
+            cost: 65
+         },
+         {
+            plat: "pasta schezuan gyuniku",
+            ingr: "pasta, res, zanahoria, calabaza, brocoli, champiñones, pimientos y cebollin.",
+            cost: 70
+         }
+      ]
+   },
+   {
+      name: "ramen",
+      imag: "media/ramen2.png",
+      data: [
+         {
+            plat: "ramen res",
+            ingr: "res, huevo, naruto, cebollin, zanahoria, calabaza, pasta ramen",
+            cost: 75
+         },
+         {
+            plat: "ramen surimi",
+            ingr: "surimi en tempura, huevo, naruto, cebollin, zanahoria, calabaza, pasta ramen.",
+            cost: 75
+         }
+      ]
+   },
+   {
+      name: "teppanyaki ",
+      imag: "media/teppanyaki.png",
+      data: [
+         {
+            plat: "teppanyaki verdura",
+            ingr: "verduras a la plancha, champiñones, calabaza, zanahoria, brocoli, pimiento, cebollin y arroz gohan.",
+            cost: 45
+         },
+         {
+            plat: "teppanyaki hiyoko (pollo)",
+            ingr: "verduras a la plancha, pollo, champiñones, calabaza, zanahoria, brocoli, pimiento, cebollin y arroz gohan.",
+            cost: 50
+         },
+         {
+            plat: "teppanyaki gyuniku",
+            ingr: "verduras a la plancha, res, champiñones, zanahoria, brocoli, pimiento y cebollin.",
+            cost: 55
+         },
+      ]
+   },
+   {
+      name: "temaki",
+      imag: "media/temaki.png",
+      data: [
+         {
+            plat: "temaki kappa shako",
+            ingr: "camaron, philadephia, pepino, zanahoria, ajonjolí.",
+            cost: 55
+         },
+         {
+            plat: "temaki vegetariano",
+            ingr: "pepino, zanahoria, aguacate, philadelphia.",
+            cost: 50
+         },
+      ]
+   },
+   {
+      name: "kushiague",
+      imag: "media/poke.png",
+      data: [
+         {
+            plat: "kushi queso",
+            ingr: "brocheta de queso manchego, philadelphia, empanizado.",
+            cost: 35
+         },
+         {
+            plat: "kushi ebi",
+            ingr: "brocheta de camaron, philadelphia, empanizado.",
+            cost: 35
+         },
+         {
+            plat: "kushi surim ",
+            ingr: "brocheta de surimi, philadelphia, empanizado.",
+            cost: 30
+         },
+         {
+            plat: "kushi banan ",
+            ingr: "brocheta de plátano frito, philadelphia, empanizado.",
+            cost: 25
+         }
+      ]
+   },
+   {
+      name: "gyozas",
+      imag: "media/gyoza.png",
+      data: [
+         {
+            plat: "gyozas",
+            ingr: "Rellenas de pollo sabor schezuan acompañado con verduras.",
+            cost: 55
+         }
+      ]
+   },
+   {
+      name: "tempura",
+      imag: "media/poke.png",
+      data: [
+         {
+            plat: "roshy roll",
+            ingr: "surimi, aguacate por fuera en tempura con pasta tampico.",
+            cost: 80
+         },
+      ]
+   },
+   {
+      name: "sushi charola",
+      imag: "media/bento.png",
+      data: [
+         {
+            plat: "sushi empanizados",
+            ingr: "charola sushi empanizado 30 piezas",
+            cost: 200
+         },
+         {
+            plat: "sushi surtidos",
+            ingr: "charola sushi empanizado y frio 40 piezas",
+            cost: 350
+         },
+         {
+            plat: "sushi surtidos",
+            ingr: "charola sushi empanizado y frio 30 piezas",
+            cost: 270
+         }
+      ]
+   },
+   {
+      name: "poke",
+      imag: "media/poke.png",
+      data: [
+         {
+            plat: "poke amai toriniku",
+            ingr: "pollo tempura en teriyaki, mango, pepino, ajonjolin, arroz gohan y alga nori.",
+            cost: 65
+         },
+         {
+            plat: "poke surimi terivyaki",
+            ingr: "surimi tempura en teriyaki, mango, pepino, ajonjolin, arroz gohan y alga nori.",
+            cost: 65
+         }
+      ]
+   },
+   {
+      name: "bebidas",
+      imag: "media/beer.png",
+      data: [
+         { plat: "nieve flotante limon", cost: 38 },
+         { plat: "jarra de te helado", ingr: "Negro", cost: 40 },
+         { plat: "vaso de te helado", ingr: "Negro", cost: 15 },
+         { plat: "jarra de calpiña", cost: 50 },
+         { plat: "vaso de calpiña", cost: 25 },
+         { plat: "refresco lata", cost: 20 },
+         { plat: "sake", cost: 15 },
+      ]
+   },
+   {
+      name: "postres",
+      imag: "media/narutomaki.png",
+      data: [
+         {
+            plat: "tempura helado classic",
+            ingr: "cubierto con chocolate líquido.",
+            cost: 40
+         },
+         {
+            plat: "tempura helado crunchy",
+            ingr: "cubierto con cajeta y nuez.",
+            cost: 45
+         },
+         {
+            plat: "tempura helado magic",
+            ingr: "cubierto con chocolate líquido, crema batida, cereza y con bola de nieve sabor chocolate.",
+            cost: 50
+         },
+         {
+            plat: "nieve de limon",
+            cost: 20
+         }
+      ]
+   },
+   {
+      name: "extras",
+      imag: "media/takoyaki.png",
+      data: [
+         { plat: "extra salsa de anguila", cost: 10 },
+         { plat: "extra aderezo de chipotle", cost: 10 },
+         { plat: "extra pasta tampco", cost: 15 },
+         { plat: "extra wasabi", cost: 10 },
+         { plat: "chiles toreados", cost: 10 },
+         { plat: "extra wasabi", cost: 10 },
+         { plat: "ingrediente extra", cost: 10 }
+      ]
+   },
+]
